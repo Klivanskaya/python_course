06a27565dbcb984c8b00bb597ec67319c827d3ae
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-06 11:08:45 +0200

    Create task4.py

---
+++ lesson2/task4.py
@@ -0,0 +1,9 @@
+import math
+num = int(input("number is: "))
+power = int(input("power is: "))
+answer = math.pow(num, power)
+another = num ** power
+print("Answer is ", answer)
+print(type(answer))
+print(another)
+print(type(another))
