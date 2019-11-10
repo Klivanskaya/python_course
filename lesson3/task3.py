82008ebfd8a89a7db84f033416b441fb38f78cdd
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-10 12:32:36 +0200

    Create task3.py

---
+++ lesson3/task3.py
@@ -0,0 +1,4 @@
+any_line = str(input("Enter any line: "))
+print('lenght of input line is:' , len(any_line))
+print('max value of input line is:' , max(any_line))
+print('min value of input line is:' , min(any_line))
