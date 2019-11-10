c0d85807bc21de239fecefb8787bdfc96afe93d1
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-10 11:30:27 +0200

    Update task2.py

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -1,6 +1,7 @@
 import random
 
-innum = random.randint(1,100)
-flonum = random.uniform(1,100)
-print(innum, flonum)
-print("Is innum bigger than flonum number?", innum > flonum )
+in_num = random.randint(1,100)
+flo_num = random.uniform(1,100)
+print("integer number is: " , in_num)
+print("float number is: ", flo_num)
+print("Is integer number bigger than float number?", in_num > flo_num )
