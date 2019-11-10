808ac627693a259f71eb56efb611cf8743bdb307
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-10 11:32:46 +0200

    Update task5.py

--- lesson2/task5.py
+++ lesson2/task5.py
@@ -1,8 +1,8 @@
 import math
 import random
-n = random.randint(1,100)
-print(n)
-square = math.pow(n, 2)
-print(square)
-print(n > 10)
-print(square > 500)
+number = random.randint(1,100)
+print("Number is: " , number)
+square = math.pow(number, 2)
+print("Square of number is: " , square)
+print("Is the number bigger than 10?" , number > 10)
+print("Is the square of number bigger than 500?" , square > 500)
