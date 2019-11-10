c0b15f4d73af01f7af29ce9b530b9358b289f64a
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-10 11:31:13 +0200

    Update task3.py

--- lesson2/task3.py
+++ lesson2/task3.py
@@ -1,7 +1,8 @@
 import random
-a = random.randint(1,100)
-b = random.randint(1,100)
-print(a)
-print(b)
-a > b
-print(a > b)
+number_a = random.randint(1,100)
+number_b = random.randint(1,100)
+print("First number is: " , number_a)
+print("Second number is: " , number_b)
+number_a > number_b
+print("Is the first number bigger than the second number?" , number_a > number_b)
+
