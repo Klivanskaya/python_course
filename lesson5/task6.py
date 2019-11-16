b3ac8530dfb6f78745489d0218181901a62fe8fb
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-16 21:50:17 +0200

    Update task6.py

--- lesson5/task6.py
+++ lesson5/task6.py
@@ -1,8 +1,7 @@
 import string
 st = tuple(dir(string))
-print(len(st))
-print('Тупл с 5 последними атрибутами модуля string: ' , st[20:25])
-result = list(st[20:25])
+print('Тупл с 5 последними атрибутами модуля string: ' , st[-5:])
+result = list(st[-5:])
 index = 2
 result.insert (index , 'capwords')
 print('Тупл с добавленной строкой capwords: ' , tuple(result))
