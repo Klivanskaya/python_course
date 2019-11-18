5dde893032136d75d1e4e756cf9cb5189ead5c29
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-18 17:28:32 +0200

    Update task4.py

--- lesson5/task4.py
+++ lesson5/task4.py
@@ -1,6 +1,6 @@
 import string
-string = input('Enter your string: ')
-string = list(sorted(string))
-my_string = " ".join(string)
-print("Елементы строки в прямом порядке: " , my_string)
-print("Елементы строки в обратном порядке: " , my_string[::-1])
+st = list(input('Enter your string: '))
+st.reverse()
+print(st)
+st.sort()
+print(st)
