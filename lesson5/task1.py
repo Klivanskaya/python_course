69ebe9242c8ef263727cd28108a33b6579aff9c6
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2019-11-19 14:58:47 +0200

    Update task1.py

--- lesson5/task1.py
+++ lesson5/task1.py
@@ -1,3 +1,4 @@
 import string
-st = dir(string.ascii_lowercase)
-print("Атрибуты модуля string по алфавиту в обратном порядке: " , st[:: - 1])
+atr = dir(string)
+atr.sort(reverse = True)
+print("Атрибуты модуля string по алфавиту в обратном порядке: " , atr)
