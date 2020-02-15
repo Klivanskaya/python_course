2012ea1644391d6a33fd8927e58378172d464685
Klivanskaya <57302660+Klivanskaya@users.noreply.github.com>
2020-02-15 13:45:42 +0200

    Update task2.py

--- lesson10/task2.py
+++ lesson10/task2.py
@@ -1,23 +1,12 @@
-with open('weather.txt', 'r')  as f:
-    one = f.readlines()
-    lst = ''.join(one).split('\n')
-    my_d = {}
-    for i in lst:
-        a = i.split(';')
-        my_d.update({a[0]:a[1]})
-    minimum = 0
-    maximum = 0
-    for k, v in my_d.items():
-        temp = 0
-        days = 0
-        if int(v) < int(minimum):
-            minimum = v
-            t = f'{k}:{minimum}'
-        elif int(v) > int(maximum):
-            maximum = v
-            m = f'{k}:{maximum}'
-        temp += int(v)
-        days += 1
-    print(t)
-    print(m)
-    print(int(temp/days))
+f = open('weather.txt')
+lst = []
+temp_list = []
+my_d = {}
+for line in f:
+    date, temp = line.strip().split(';')
+    lst.append((date, int(temp)))
+    temp_list.append(int(temp))
+    my_d[int(temp)] = date
+print(min(temp_list), max(temp_list))
+print(sum(temp_list)/len(temp_list))
+print(len(my_d))
